d69b3d673521c801dd8db7bab468835f067aadd6
zealotzuo <zealotzuo@f19589de-7a3d-11de-a624-ab7c8f4ec817>
2009-07-27 04:53:12 +0000

    Added step-by-step function

--- dtmsim.c
+++ dtmsim.c
@@ -1,7 +1,24 @@
+/*
+	DETERMINISTIC TURING MACHINE SIMULATOR
+	COPYRIGHT (C) ZEE ZUO 2009
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	ALONG WITH this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 #define DIRECTION int
 #define LEFT -1
 #define RIGHT 1
@@ -21,19 +38,66 @@ int nrules;
 struct Rule rules[1001];
 char tape[10001];
 
+int sbys;
+int leftmost, rightmost;
+
+void pressenter()
+{
+	char buffer[10001];
+	printf("Press enter to continue.");
+	fgets(buffer, 10000, stdin);
+}
+
 int runTM(int head, int state)
 {
-	int i;
+	int i, j, thb, nh;
 	
 	for(i=1; i<=nrules; ++i)
 	{
 		if(rules[i].forstate != state) continue;
 		if(rules[i].fortape != tape[head]) continue;
+		thb = tape[head];
 		tape[head] = rules[i].totape;
-		return runTM(head + rules[i].direction, rules[i].tostate);
+		nh = head + rules[i].direction;
+		if(head<leftmost) leftmost = head;
+		if(head>rightmost) rightmost = head;
+		if(sbys)
+		{
+			printf("\nFor state %d and tape symbol %c, write %c, go to state %d, and move to ",
+				state, thb, rules[i].totape, rules[i].tostate);
+			if(nh > head) printf("right"); else printf("left"); printf(".\n");
+			if(nh != leftmost) for(j=1; j<=(nh-leftmost)*2; ++j) printf(" ");
+			printf("H\n");
+			for(j=leftmost; j<=rightmost; ++j) printf("%c ", tape[j]);
+			printf("\n");
+			pressenter();
+			printf("\n");
+		}
+
+		return runTM(nh, rules[i].tostate);
 	}
 
 	return state;
+}
+
+void unknowndie()
+{
+	printf("Unknown command: Type -h for help.\n");
+	exit(1);
+}
+
+void nofiledie()
+{
+	printf("Cannot find specified file.\n");
+	exit(1);
+}
+
+void printhelp()
+{
+	printf("Usage: dtmsim [rulefile] [input_tape]\n\n");
+	printf("rulefile: A file representing the simulated Turing machine. See <http://dtmsim.googlecode.com/> for more information.\n");
+	printf("input_tape: The input tape. This should not include the character \'B\', which is used for blank symbol.");
+	exit(0);
 }
 
 int main(int argc, char** argv)
@@ -48,9 +112,15 @@ int main(int argc, char** argv)
 
 	for(i=1; i<=10000; ++i) tape[i] = BLANK;
 
-	if(argc != 3) return -1;
+	if((!strcmp(argv[1], "-h"))||(!strcmp(argv[1], "--help"))) printhelp();
 	frule = fopen(argv[1], "r");
-	if(frule == NULL) return -1;
+	if(frule == NULL) nofiledie();
+	
+	sbys = 0;
+	if(argc >= 4) for(i=3; i<argc; ++i)
+	{
+		if(!strcmp(argv[i], "-s")) sbys = 1;
+	}
 
 	fscanf(frule, "%d %d", &nstates, &naccp);
 	for(i=1; i<=naccp; ++i) fscanf(frule, "%d", accp+i);
@@ -70,6 +140,16 @@ int main(int argc, char** argv)
 	for(i=0; i<strlen(argv[2]); ++i)
 	{
 		tape[TSTART+i] = argv[2][i];
+	}
+
+	leftmost = TSTART;
+	rightmost = TSTART + strlen(argv[2]) - 1;
+	if(sbys)
+	{
+		printf("Start in state 0 and tape symbol %c.\n", tape[TSTART]);
+		printf("H\n");
+		for(i=leftmost; i<=rightmost; ++i) printf("%c ", tape[i]); printf("\n");
+		pressenter();
 	}
 
 	finalstate = runTM(TSTART, 0);
